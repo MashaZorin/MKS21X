71f242c4a41558f46ee30545b2a05705ec9059de
Masha Zorin <masha.zorin@gmail.com>
2016-12-13 06:39:08 -0500

    Test written, not tested yet

--- 04Sorts/Sorts.java
+++ 04Sorts/Sorts.java
@@ -1,28 +1,35 @@
 public class Sorts{
-  /**EDIT THIS METHOD TO MATCH YOUR NAME 
-  */
-  public static String name(){
-    return "01.Zorin.Masha"; 
-  }
+    /**EDIT THIS METHOD TO MATCH YOUR NAME 
+     */
+    public static String name(){
+	return "01.Zorin.Masha"; 
+    }
 
-  /**Selection sort of an int array. 
-  *Upon completion, the elements of the array will be in increasing order.
-  *@param data  the elements to be sorted.
-  */
-  public static void selectionSort(int[] data){
-      for (int i = 0; i < data.size() - 1; i ++){
-	  int smallest = i;
-	  for (int j = i; j < data.size(); j ++){
-	      if (data.[smallest] > data[j]){
-		  smallest = j;
-	      }
-	  }
-	  if (smallest != 1){
-	      int placeholder = data[i];
-	      data[i] = data[smallest];
-	      data[smallest] = placeholder;
-	  }
-      }
-  }
+    /**Selection sort of an int array. 
+     *Upon completion, the elements of the array will be in increasing order.
+     *@param data  the elements to be sorted.
+     */
+    public static void selectionSort(int[] data){
+	for (int i = 0; i < data.length - 1; i ++){
+	    int smallest = i;
+	    for (int j = i; j < data.length; j ++){
+		if (data[smallest] > data[j]){
+		    smallest = j;
+		}
+	    }
+	    if (smallest != 1){
+		int placeholder = data[i];
+		data[i] = data[smallest];
+		data[smallest] = placeholder;
+	    }
+	}
+    }
+
+    public static void main(String[]args){
+	int[] ary = {64,25,12,34,11};
+	System.out.println(ary);
+	selectionSort(ary);
+	System.out.println(ary);
+    }
 
 }
---
+++ 04Sorts/Sorts.java~
@@ -0,0 +1,28 @@
+public class Sorts{
+  /**EDIT THIS METHOD TO MATCH YOUR NAME 
+  */
+  public static String name(){
+    return "01.Zorin.Masha"; 
+  }
+
+  /**Selection sort of an int array. 
+  *Upon completion, the elements of the array will be in increasing order.
+  *@param data  the elements to be sorted.
+  */
+  public static void selectionSort(int[] data){
+      for (int i = 0; i < data.size() - 1; i ++){
+	  int smallest = i;
+	  for (int j = i; j < data.size(); j ++){
+	      if (data.[smallest] > data[j]){
+		  smallest = j;
+	      }
+	  }
+	  if (smallest != 1){
+	      int placeholder = data[i];
+	      data[i] = data[smallest];
+	      data[smallest] = placeholder;
+	  }
+      }
+  }
+
+}
